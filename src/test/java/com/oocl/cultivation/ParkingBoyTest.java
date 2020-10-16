f898972a3a65a2e1fcbb5853657e44b8728762ca
Cedric Don Coloma <colomce@oocl.com>
2020-10-16 13:57:50 +0800

    --message=[ParkingBoy][Cedric Coloma] test: Create test that returns Ticket when ParkingBoy park a Car

--- src/test/java/com/oocl/cultivation/ParkingBoyTest.java
+++ src/test/java/com/oocl/cultivation/ParkingBoyTest.java
@@ -2,9 +2,20 @@ package com.oocl.cultivation;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
 class ParkingBoyTest {
     @Test
-    void write_your_first_test() {
+    void should_return_ParkingTicket_when_ParkingBoy_park_Car_given_ParkingBoy_and_a_Car() {
+        //given
+        ParkingBoy parkingBoy = new ParkingBoy();
+        Car car = new Car();
 
+        //when
+        ParkingTicket parkingTicket = new ParkingTicket();
+        parkingTicket = parkingBoy.park(car);
+
+        //then
+        assertNotNull(parkingTicket);
     }
 }
