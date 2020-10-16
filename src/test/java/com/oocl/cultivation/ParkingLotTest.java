11a597b05e7c56252ea3a13aeff6cf914b9896bd
Cedric Don Coloma <colomce@oocl.com>
2020-10-16 14:33:50 +0800

    [ParkingLot][Cedric Coloma] refactor: Removed unnecessary test for ParkingLot class

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -1,18 +1,5 @@
 package com.oocl.cultivation;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class ParkingLotTest {
 
-    @Test
-    void should_return_ParkingLot_when_new_ParkingLot_given_a_ParkingLot() {
-        //given
-        ParkingLot parkingLot = new ParkingLot();
-
-        //when
-
-        //then
-        Assertions.assertEquals(parkingLot.getClass(), ParkingLot.class);
-    }
 }
