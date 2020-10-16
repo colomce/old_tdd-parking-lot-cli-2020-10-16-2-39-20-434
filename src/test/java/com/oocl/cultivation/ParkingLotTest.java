a5187b59900e0da3aac2149f260f3fd869baff76
Cedric Don Coloma <colomce@oocl.com>
2020-10-16 14:52:55 +0800

    [ParkingLot][Cedric Coloma] test: Check if ParkingLot returns a ticket after calling park

--- src/test/java/com/oocl/cultivation/ParkingLotTest.java
+++ src/test/java/com/oocl/cultivation/ParkingLotTest.java
@@ -1,5 +1,20 @@
 package com.oocl.cultivation;
 
-public class ParkingLotTest {
+import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class ParkingLotTest {
+    @Test
+    void should_return_ParkingTicket_when_park_given_ParkingLot_and_Car() {
+        //given
+        ParkingLot parkingLot = new ParkingLot();
+        Car car = new Car();
+
+        //when
+        ParkingTicket parkingTicket = parkingLot.park(car);
+
+        //then
+        assertNotNull(parkingTicket);
+    }
 }
