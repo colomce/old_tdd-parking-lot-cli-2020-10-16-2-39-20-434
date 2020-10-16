f73acd3e88fff55f720fa2c829a1fc5ccc33175c
Cedric Don Coloma <colomce@oocl.com>
2020-10-16 14:32:34 +0800

    [ParkingLot][Cedric Coloma] refactor: Removed unnecessary test for Car class

--- src/test/java/com/oocl/cultivation/CarTest.java
+++ src/test/java/com/oocl/cultivation/CarTest.java
@@ -1,16 +1,5 @@
 package com.oocl.cultivation;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
-
 public class CarTest {
-    @Test
-    void should_return_new_Car_when_new_Car_given_Car() {
-        //given
-        Car car = new Car();
-        //when
 
-        //then
-        Assertions.assertEquals(car.getClass(), Car.class);
-    }
 }
